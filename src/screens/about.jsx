87c9f3189292a5d746d250bfbfd9e1cfe92e44f0
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-07-24 20:57:30 +0500

    data fetching base done

--- src/screens/about.jsx
+++ src/screens/about.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import useSWR from "swr";
+import { Link } from "../Components/Link";
+import { fetcher } from "../utils/fetcher";
 
 export default function about() {
-  return <div>about</div>;
+  const { data } = useSWR(
+    "https://api.github.com/users/MehfoozurRehman/repos",
+    fetcher,
+    { suspense: true }
+  );
+  return (
+    <div>
+      about
+      <Link to="/">Home</Link>
+      <ul>
+        {data.map((repo) => (
+          <li key={repo.id}>{JSON.stringify(repo)}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
