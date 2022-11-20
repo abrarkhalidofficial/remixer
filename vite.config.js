17be17746919d9ef9e40474e560feb57ed0a4dd7
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-11-20 19:23:46 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { imagetools } from "vite-imagetools";
-import viteImagemin from "vite-plugin-imagemin";
-import viteCompression from "vite-plugin-compression";
-import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,7 +21,7 @@ export default defineConfig({
       },
       {
         find: "router",
-        replacement: path.resolve(__dirname, "./src/Router.jsx"),
+        replacement: path.resolve(__dirname, "./src/router.jsx"),
       },
     ],
   },
