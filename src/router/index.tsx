ec34b2566675296a76c159125cec0da06214a92f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-04 16:08:58 +0500

    added react-click-away-listener and jotai

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,4 +3,3 @@ export { default as Router } from "./Router";
 export { default as Head } from "./Head";
 export { default as Link } from "./Link";
 export { default as NavLink } from "./NavLink";
-export { default as ClickAwayListner } from "./ClickAwayListner";
