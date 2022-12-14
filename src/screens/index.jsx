cd3076012e15cdb416f64657ee52342ee6648d9c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-15 03:14:12 +0500

    clean up

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,8 +1,3 @@
-import { useLoaderData } from "react-router-dom";
-
-export const loader = () => "index";
-
 export default function Index() {
-  const data = useLoaderData();
-  return <div>{data}</div>;
+  return <div>index</div>;
 }
