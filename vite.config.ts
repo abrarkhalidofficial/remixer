1e827badb71a028cbc52b4fc4aa8c0dcf15a1258
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-31 02:45:50 +0500

    house keeping

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import HotExport from "vite-plugin-hot-export";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 import { defineConfig } from "vite";
+import hotExport from "vite-plugin-hot-export";
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
 import viteImagemin from "vite-plugin-imagemin";
+import vitePluginBrowserSync from "vite-plugin-browser-sync";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,9 +32,10 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    ViteWebfontDownload(),
+    hotExport(),
     chunkSplitPlugin(),
-    HotExport(),
+    ViteWebfontDownload(),
+    vitePluginBrowserSync(),
     viteCompression({
       algorithm: "brotliCompress",
       threshold: 100,
