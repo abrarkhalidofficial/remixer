8ab90e5e0ec4e0232d27ace054a4c7214d8b8f77
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-05 03:21:49 +0500

    clean up

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,6 @@ import viteImagemin from "vite-plugin-imagemin";
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
-  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
-
   return defineConfig({
     resolve: {
       alias: [
