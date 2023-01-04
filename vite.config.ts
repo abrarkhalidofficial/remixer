0afb3df5dd21116560d131fd42ae9a9eccfb1f7a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-05 03:18:50 +0500

    added a way to switch between csr and ssrusing env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+import { defineConfig, loadEnv } from "vite";
+
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 import { chunkSplitPlugin } from "vite-plugin-chunk-split";
-import { defineConfig } from "vite";
 import hotExport from "vite-plugin-hot-export";
 import mix from "vite-plugin-mix";
 import path from "path";
@@ -8,83 +9,90 @@ import react from "@vitejs/plugin-react-swc";
 import viteCompression from "vite-plugin-compression";
 import viteImagemin from "vite-plugin-imagemin";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: "components",
-        replacement: path.resolve(__dirname, "./src/components"),
-      },
-      {
-        find: "assets",
-        replacement: path.resolve(__dirname, "./src/assets"),
-      },
-      {
-        find: "global",
-        replacement: path.resolve(__dirname, "./src/global"),
-      },
-      {
-        find: "router",
-        replacement: path.resolve(__dirname, "./src/router"),
-      },
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+
+  return defineConfig({
+    resolve: {
+      alias: [
+        {
+          find: "components",
+          replacement: path.resolve(__dirname, "./src/components"),
+        },
+        {
+          find: "assets",
+          replacement: path.resolve(__dirname, "./src/assets"),
+        },
+        {
+          find: "global",
+          replacement: path.resolve(__dirname, "./src/global"),
+        },
+        {
+          find: "router",
+          replacement: path.resolve(__dirname, "./src/router"),
+        },
+      ],
+    },
+    plugins: [
+      react(),
+      hotExport(),
+      chunkSplitPlugin(),
+      ViteWebfontDownload(),
+      process.env.VITE_BUILD_MODE === "ssr"
+        ? mix.default({
+            handler: "./src/layouts/api.jsx",
+          })
+        : null,
+      viteCompression({
+        algorithm: "brotliCompress",
+        threshold: 100,
+      }),
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 7,
+        },
+        mozjpeg: {
+          quality: 30,
+        },
+        pngquant: {
+          quality: [0.7, 0.8],
+          speed: 4,
+        },
+        webp: {
+          quality: 70,
+        },
+        svgo: {
+          multipass: true,
+          plugins: [
+            {
+              name: "removeViewBox",
+            },
+            {
+              name: "minifyStyles",
+            },
+            {
+              name: "removeMetadata",
+            },
+            {
+              name: "removeUselessStrokeAndFill",
+            },
+            {
+              name: "reusePaths",
+            },
+            {
+              name: "removeEmptyAttrs",
+              active: true,
+            },
+          ],
+        },
+      }),
     ],
-  },
-  plugins: [
-    react(),
-    hotExport(),
-    chunkSplitPlugin(),
-    ViteWebfontDownload(),
-    mix.default({
-      handler: "./src/layouts/api.jsx",
-    }),
-    ,
-    viteCompression({
-      algorithm: "brotliCompress",
-      threshold: 100,
-    }),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 30,
-      },
-      pngquant: {
-        quality: [0.7, 0.8],
-        speed: 4,
-      },
-      webp: {
-        quality: 70,
-      },
-      svgo: {
-        multipass: true,
-        plugins: [
-          {
-            name: "removeViewBox",
-          },
-          {
-            name: "minifyStyles",
-          },
-          {
-            name: "removeMetadata",
-          },
-          {
-            name: "removeUselessStrokeAndFill",
-          },
-          {
-            name: "reusePaths",
-          },
-          {
-            name: "removeEmptyAttrs",
-            active: true,
-          },
-        ],
-      },
-    }),
-  ],
-});
+  });
+};
