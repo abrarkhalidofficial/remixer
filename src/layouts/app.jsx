591133ba707d10e21509ce81d0aa7c4fc9f4f248
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-05 06:41:10 +0500

    Head changes done

--- src/layouts/app.jsx
+++ src/layouts/app.jsx
@@ -1,10 +1,16 @@
-import { Footer, Header } from "../components";
+import { Footer, Header } from "components";
 
+import { Head } from "router";
 import { Outlet } from "react-router-dom";
 
 export default function App() {
+  const title = "Vite file router";
+  const description = "Vite file router";
+  const image = "/vite.svg";
+  const url = "https://vitefilerouter.com";
   return (
     <>
+      <Head title={title} image={image} url={url} description={description} />
       <Header />
       <Outlet />
       <Footer />
