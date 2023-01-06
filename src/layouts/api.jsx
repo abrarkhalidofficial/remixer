19ba83c9f076fe21adb2b9e139a426a7df56885f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-06 10:50:36 +0500

    api handlers auto picker done

--- src/layouts/api.jsx
+++ src/layouts/api.jsx
@@ -1,9 +1,16 @@
 import express from "express";
 
+const API_ROUTES = import.meta.glob("/src/handlers/**/[a-z[]*.(js|ts)", {
+  eager: true,
+});
+const apiRoutes = Object.keys(API_ROUTES).map(
+  (route) => API_ROUTES[route].default
+);
+
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.send("Hello World!");
+apiRoutes.map((route) => {
+  app.use("/api/", route);
 });
 
 export const handler = app;
