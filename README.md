66a8e40b27662a5fd2fabfe0862e2c434740114d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-01-25 23:03:51 +0500

    clean up

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
 
-A starter for react apps to make development faster it has file based routing, flaten routes, lazy routes, protected routes, nested routes, asset optimization, auto component & asset export, auto style imports for sass or css, ssr and csr support, optional express handler for developing apis, prefetching support, head component
+A starter for react apps to make development faster it has file based routing, flaten routes, lazy routes, protected routes, nested routes, asset optimization, auto component & asset export, auto style imports for sass or css, prefetching support, head component
 
 just wanted to make nextjs like features but liked remix styles better so here it is 💖
 
@@ -18,31 +18,11 @@ just wanted to make nextjs like features but liked remix styles better so here i
 
 #### `yarn install`
 
-installing dependencies
-
 #### `yarn dev`
 
-starting a development server
-
-for ssr
-
-#### `yarn build with VITE_BUILD_MODE=ssr in .env`
-
-for csr
-
-#### `yarn build with VITE_BUILD_MODE=csr in .env`
-
-making a production build
-
-for ssr
-
-#### `yarn start`
-
-for csr
+#### `yarn build`
 
 #### `yarn preview`
-
-starting a production server
 
 ## License
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,95 +1,84 @@
-import { defineConfig, loadEnv } from "vite";
-
+import { defineConfig} from "vite";
 import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
 import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 import hotExport from "vite-plugin-hot-export";
-import mix from "vite-plugin-mix";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
 import viteImagemin from "vite-plugin-imagemin";
 
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    resolve: {
-      alias: [
-        {
-          find: "components",
-          replacement: path.resolve(__dirname, "./src/components"),
-        },
-        {
-          find: "assets",
-          replacement: path.resolve(__dirname, "./src/assets"),
-        },
-        {
-          find: "global",
-          replacement: path.resolve(__dirname, "./src/global"),
-        },
-        {
-          find: "router",
-          replacement: path.resolve(__dirname, "./src/router"),
-        },
-      ],
-    },
-    plugins: [
-      react(),
-      hotExport(),
-      chunkSplitPlugin(),
-      ViteWebfontDownload(),
-      viteCompression({
-        algorithm: "brotliCompress",
-        threshold: 100,
-      }),
-      viteImagemin({
-        gifsicle: {
-          optimizationLevel: 7,
-          interlaced: false,
-        },
-        optipng: {
-          optimizationLevel: 7,
-        },
-        mozjpeg: {
-          quality: 30,
-        },
-        pngquant: {
-          quality: [0.7, 0.8],
-          speed: 4,
-        },
-        webp: {
-          quality: 70,
-        },
-        svgo: {
-          multipass: true,
-          plugins: [
-            {
-              name: "removeViewBox",
-            },
-            {
-              name: "minifyStyles",
-            },
-            {
-              name: "removeMetadata",
-            },
-            {
-              name: "removeUselessStrokeAndFill",
-            },
-            {
-              name: "reusePaths",
-            },
-            {
-              name: "removeEmptyAttrs",
-              active: true,
-            },
-          ],
-        },
-      }),
-      process.env.VITE_BUILD_MODE === "ssr"
-        ? mix.default({
-            handler: "./src/layouts/api.jsx",
-          })
-        : null,
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "components",
+        replacement: path.resolve(__dirname, "./src/components"),
+      },
+      {
+        find: "assets",
+        replacement: path.resolve(__dirname, "./src/assets"),
+      },
+      {
+        find: "global",
+        replacement: path.resolve(__dirname, "./src/global"),
+      },
+      {
+        find: "router",
+        replacement: path.resolve(__dirname, "./src/router"),
+      },
     ],
-  });
-};
+  },
+  plugins: [
+    react(),
+    hotExport(),
+    chunkSplitPlugin(),
+    ViteWebfontDownload(),
+    viteCompression({
+      algorithm: "brotliCompress",
+      threshold: 100,
+    }),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 30,
+      },
+      pngquant: {
+        quality: [0.7, 0.8],
+        speed: 4,
+      },
+      webp: {
+        quality: 70,
+      },
+      svgo: {
+        multipass: true,
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "minifyStyles",
+          },
+          {
+            name: "removeMetadata",
+          },
+          {
+            name: "removeUselessStrokeAndFill",
+          },
+          {
+            name: "reusePaths",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: true,
+          },
+        ],
+      },
+    }),
+  ],
+});
