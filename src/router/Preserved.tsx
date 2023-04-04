c692eeabb21917e8ea461bc0519bfd9b07cf8d1f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-04-04 07:47:02 +0500

    optimizations for preserved routes

--- src/router/Preserved.tsx
+++ src/router/Preserved.tsx
@@ -1,23 +1,26 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-const PRESERVED = import.meta.glob(
-  "/src/layouts/(App|NotFound|Loading).(jsx|tsx)",
-  { eager: true }
-);
+const PRESERVED = import.meta.glob('/src/layouts/(App|NotFound|Loading).(jsx|tsx)', { eager: true });
 
 const preserved = Object.keys(PRESERVED).reduce(
-  (preserved, file) => ({
-    ...preserved,
+  (acc, file) => ({
+    ...acc,
     [file.replace(/\/src\/layouts\/|\.jsx|\.tsx$/g, "")]:
       PRESERVED[file].default,
   }),
   {}
 );
 
-if (!Object.keys(preserved).includes("NotFound"))
-  console.error("No 404 element found");
-if (!Object.keys(preserved).includes("Loading"))
-  console.error("No loader function found");
+const hasNotFoundError = !Object.keys(preserved).includes("NotFound")
+const hasLoadingError = !Object.keys(preserved).includes("Loading")
+
+if (hasNotFoundError || hasLoadingError) {
+  const message =
+    (hasNotFoundError ? "No 404 element found; " : "") +
+    (hasLoadingError ? "No loader function found" : "");
+
+  console.error(message);
+}
 
 export const App = preserved?.["App"] || Fragment;
 export const NotFound = preserved?.["NotFound"] || Fragment;
